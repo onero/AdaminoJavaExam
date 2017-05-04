20b5edbb74e65060521c7eaa63cfcdc5eea864ba
Adam Hansen <adamlars90@gmail.com>
2017-05-04 08:47:08 +0200

    Updated menu

--- src/adaminojavaexam/gui/view/UserMenu.java
+++ src/adaminojavaexam/gui/view/UserMenu.java
@@ -13,6 +13,7 @@ public class UserMenu {
      * @return
      */
     public static int getUserChoice() {
+        System.out.println();
         System.out.println("Please select one of the following options: ");
         System.out.println("0) Exit");
         System.out.println("1) Add song");
